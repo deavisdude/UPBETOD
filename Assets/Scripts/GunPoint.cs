aa7495b9b5e325e724180cf088c9d80f5838cf4a
Davis Odom <deavisdude@gmail.com>
2015-01-23 23:20:53 -0500

    Gun Points at cursor

---
+++ Assets/Scripts/GunPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunPoint : MonoBehaviour {
+
+	public GameObject pointAt;
+	float x;
+	float y;
+	float angle;
+
+	void Update () {
+		Vector3 diff = pointAt.transform.position - transform.position;
+		diff.Normalize();
+		
+		float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+	}
+}

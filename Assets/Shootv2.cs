186e248720c02104ac0e5263aa59f9e3c84d9e63
Alek <alekcangel@yahoo.com>
2015-01-24 02:50:34 -0500

    hgiklkfckl

--- Assets/Shootv2.cs
+++ Assets/Shootv2.cs
@@ -1,37 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/*public class Shootv2 : MonoBehaviour {
-	
-	public rigidbody2D bullet;
-	public float velocity = 10.0f;
-
-	void Update ()
-	{
-		if(Input.GetButtonDown("Fire1"))
-		{
-			rigidbody2D newBullet = Instantiate(bullet,transform.position,transform.rotation) as rigidbody2D;
-			newBullet.AddForce(transform.forward*velocity,ForceMode.VelocityChange);
-		}
-	}
-}
-
-public class Shootv2 : MonoBehaviour {
-	public Rigidbody2D bullet; // Prefab of the bullet.
-	public float bulletspeed = 20f; // The speed of the bullet
-	public Vector3 Rotation; // Vector3 to save rotation
-	
-	void Update ()
-	{
-		if(Input.GetButtonDown("Fire1"))
-		{
-			//Instantiate!
-			Rigidbody2D bulletInstance = Instantiate(bullet, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
-			bulletInstance.velocity = transform.forward * bulletspeed;
-		}
-	}
-}*/
-
 
 public class Shootv2 : MonoBehaviour {
 

f88ccec043aa4389d4dfb62c00d8f709b4c71b3a
Davis Odom <deavisdude@gmail.com>
2015-01-23 23:32:43 -0500

    jump

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -4,20 +4,22 @@ using System.Collections;
 public class MovePlayer : MonoBehaviour {
 	public float speed = 10f;
 	public float maxSpeed = 6;
+	public LayerMask layer;
+	public Transform groundCheck;
 
 	void Start(){
 
 	}
 
 	void Update () {
+
 		if(Input.GetKey(KeyCode.D) && rigidbody2D.velocity.x < maxSpeed)
 			rigidbody2D.AddForce(transform.right * (maxSpeed -rigidbody2D.velocity.x));
 
 		else if(Input.GetKey(KeyCode.A) && (-rigidbody2D.velocity.x < maxSpeed))
 			rigidbody2D.AddForce(-transform.right* (-(-maxSpeed - rigidbody2D.velocity.x)));
-		//if(rigidbody2D.velocity.x > 6)
-			//rigidbody2D.velocity = new Vector2(6,rigidbody2D.velocity.y);
 
-
+		if(Input.GetKeyDown(KeyCode.Space) && Physics2D.Linecast(transform.position, groundCheck.position,layer))
+			rigidbody2D.AddForce(new Vector2(0,speed),ForceMode2D.Impulse);
 	}
 }

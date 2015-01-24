4f05d4c2163f0ee4ac10266795dde40766cef652
Davis Odom <deavisdude@gmail.com>
2015-01-23 22:38:57 -0500

    Added crosshair and mouse binding

---
+++ Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFollow : MonoBehaviour {
+
+	void Update () {
+		transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.Translate (new Vector3(0,0,10));
+	}
+}

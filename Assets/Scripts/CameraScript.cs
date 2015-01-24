a5bc9c1f64d515d30feb9fcda3ca3c7d140aab28
Justin Walters <justinrwalters95@gmail.com>
2015-01-24 01:09:36 -0500

    Ignore

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -2,21 +2,26 @@
 using System.Collections;
 
 public class CameraScript : MonoBehaviour {
-	
-	public float dampTime = 0.15f;
-	private Vector3 velocity = Vector3.zero;
-	public Transform target;
-	
+	/*public int numPlayers;
+	public Transform[] players;
 	// Update is called once per frame
+	void Start()
+	{
+		players = new Transform[numPlayers];
+	}
 	void Update () 
 	{
-		if (target)
-		{
-			Vector3 point = camera.WorldToViewportPoint(target.position);
-			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
-			Vector3 destination = transform.position + delta;
-			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		for (int i = 0; i<numPlayers; i++) {
+			int farthestDist = 0;
+			for(int i = 0;i < numPlayers;i++)
+			{
+				if((Mathf.Abs (transform.position.x)-Mathf.Abs(players[i].position.x)) > farthestDist)
+				{
+					farthestDist = (Mathf.Abs (transform.position.x)-Mathf.Abs(players[i].position.x));
+				}
+			}
+		
 		}
 		
-	}
+	}*/
 }

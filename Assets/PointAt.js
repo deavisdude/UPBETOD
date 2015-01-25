5ddb7e4e2f3da87c9f7981f1e04ffcd719679e61
parameciumkid <parameciumkid@aim.com>
2015-01-25 02:39:20 -0500

    black hole workaround doohickey

---
+++ Assets/PointAt.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var target : Transform;
+var lockTo : Vector3 = Vector3.zero;
+
+
+function Update () {
+if(lockTo.sqrMagnitude <= 0f)	lockTo = transform.up;
+transform.rotation = Quaternion.LookRotation(target.position - transform.position, lockTo);
+}

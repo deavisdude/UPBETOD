4c93554736191a90910143196b86c810696443c4
Justin Walters <justinrwalters95@gmail.com>
2015-01-25 01:59:46 -0500

    Tri-Shot Tester

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+
+		void OnBecameInvisible() {
+			Destroy(gameObject);
+		}
+		
+		void OnTriggerEnter() {
+			Destroy(gameObject);
+		}
+	}
+	
--- Assets/Scripts/Shootv2.cs
+++ Assets/Scripts/Shootv2.cs
@@ -9,36 +9,76 @@ public class Shootv2 : MonoBehaviour {
 	public bool shootToggle;
 	public float fireRate = 1;
 	private float afireRate;
-
+	public int typeOfShot;
 	void Start()
 	{
 		shootToggle = true;
+		//index 0 = Standard ammon, 1 = triShot , 2 = StraightShot, 3 = CannonShot, 4 = Machine Gun
+		typeOfShot = 0;
 	}
 
 	void Update ()
 	{
-						if (shootToggle) {
-								if (Input.GetButtonDown ("Fire1")) {
+				//Debug, 1-4 change ammo type
+				if (Input.GetKeyDown (KeyCode.Alpha1)) {
+						typeOfShot = 0;		
+				}
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			typeOfShot = 1;		
+		}
+				if (shootToggle) {
+						if (Input.GetButtonDown ("Fire1")) {
+								if (typeOfShot == 0) {
 										shootDirection = Input.mousePosition; //set direction to where the mouse is initially
 										shootDirection.z = 0.0f; //Cure users somehow clicking in the z axis
 		
 										shootDirection = Camera.main.ScreenToWorldPoint (shootDirection); 
 
 										shootDirection = shootDirection - transform.position;
-				shootDirection.Normalize();
+										shootDirection.Normalize ();
 										Rigidbody2D bulletInstance = Instantiate (bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
 										bulletInstance.velocity = new Vector2 (shootDirection.x * speed, shootDirection.y * speed);
 										
-										Debug.Log (bulletInstance.velocity.ToString());
-										Physics2D.IgnoreCollision(transform.parent.collider2D,bulletInstance.collider2D);
+										Debug.Log (bulletInstance.velocity.ToString ());
+										Physics2D.IgnoreCollision (transform.parent.collider2D, bulletInstance.collider2D);
 										shootToggle = false;
 
 										if (IsInvoking () == false) {
 												Invoke ("shootCD", fireRate);
 										}
 								}
+								else if(typeOfShot ==1)
+								{
+									shootDirection = Input.mousePosition; //set direction to where the mouse is initially
+									shootDirection.z = 0.0f; //Cure users somehow clicking in the z axis
+									
+									shootDirection = Camera.main.ScreenToWorldPoint (shootDirection); 
+									
+									shootDirection = shootDirection - transform.position;
+									shootDirection.Normalize ();
+
+									Rigidbody2D bulletInstance = Instantiate (bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+									Rigidbody2D bulletInstance2 = Instantiate (bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+									Rigidbody2D bulletInstance3 = Instantiate (bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
+									Vector2 right = new Vector2(shootDirection.x-15,shootDirection.y+45);
+									bulletInstance.velocity = new Vector2 ((right.x) * speed, (right.y) * speed);
+									bulletInstance2.velocity = new Vector2 (shootDirection.x * speed, shootDirection.y * speed);
+									bulletInstance3.velocity = new Vector2 (shootDirection.x* speed, shootDirection.y * speed);
+
+									
+									Physics2D.IgnoreCollision (transform.parent.collider2D, bulletInstance.collider2D);
+									Physics2D.IgnoreCollision (transform.parent.collider2D, bulletInstance2.collider2D);
+									Physics2D.IgnoreCollision (transform.parent.collider2D, bulletInstance3.collider2D);
+
+									shootToggle = false;
+									
+									if (IsInvoking () == false) {
+										Invoke ("shootCD", fireRate);
+									}
+								}
 						} 
 				}
+		}
 
 
 void shootCD()
